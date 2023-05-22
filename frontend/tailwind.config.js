934b1c1eca3aa5548971438cfe9f60778705abb6
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-22 16:33:03 +0800

    feat: Configure Tailwind and Daisy UI
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        isko: {
+          "primary": "#8A1538",
+          "secondary": "#00573F",
+          "accent": "#BC8816",
+          "neutral": "#3D4451",
+          "base-100": "#ffffff",
+          "base-200": "#f2f2f2",
+          "base-300": "#e7e7e7",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#B71c1c",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+}
+
