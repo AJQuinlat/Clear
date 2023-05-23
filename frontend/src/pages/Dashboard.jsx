54da965f05ff14f518c4d53542af126ca4144634
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-23 17:15:47 +0800

    feat: Introduce account menu UI
    Initial work for the user account menu
    
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -47,31 +47,57 @@ class Dashboard extends React.Component {
     render() {
         return (
             <page>
-                <section className="navbar">
+                <div className="h-1" />
+                <section className="navbar px-8">
                     <div className="flex-1">
                         <button className="btn btn-ghost text-primary normal-case text-xl">Clear</button>
-                    </div>
+                    </div>  
                     <div className="flex-none gap-2">
-                        <div className="form-control">
-                            <input type="text" placeholder="Search" className="input input-bordered" />
-                        </div>
                         <div className="dropdown dropdown-end">
-                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                                <div className="w-10 rounded-full">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar" id="menuButton">
+                                <div className="w-12 rounded-full">
                                     <img src="https://placehold.jp/150x150.png" />
                                 </div>
                             </label>
-                            <ul tabIndex={0} className="mt-3 py-12 px-12 shadow menu dropdown-content bg-base-100 rounded-box w-80">
-                                <section className="m-auto">
-                                    <div className="w-20 avatar">
+                            <ul tabIndex={0} className="mt-3 pt-10 pb-4 px-8 shadow-lg menu dropdown-content bg-base-100 rounded-box w-80">
+                                <section className="flex flex-col m-auto">
+                                    <div className="mb-4 m-auto w-20 avatar">
                                         <img className="rounded-full" src="https://placehold.jp/150x150.png" />
                                     </div>
+                                    <span className="text-lg text-center font-semibold m-auto">John Vincent M. Corcega</span>
+                                    <span className="text-xs text-center opacity-60 m-auto">2021-04240</span>
+                                    <span className="text-xs text-center opacity-60 m-auto">Student - Second Year (2nd Semester)</span>
+                                    <div className="h-8" />
+                                    <span className="text-md text-start font-semibold m-start">Assigned Adviser</span>
+                                    <div className="flex mt-3">
+                                        <div className="flex-none w-10 avatar">
+                                            <img className="rounded-full" src="https://placehold.jp/150x150.png" />
+                                        </div>
+                                        <div className="flex flex-col flex-auto ml-4">
+                                            <span className="text-md font-semibold">Arian J. Jacildo</span>
+                                            <span className="text-xs text-light">ajjacildo@up.edu.ph</span>
+                                        </div>
+                                    </div>
+                                    <div className="h-6" />
+                                    <span className="text-md text-start font-semibold m-start">Assigned Clearance Officer</span>
+                                    <div className="flex mt-3">
+                                        <div className="flex-none w-10 avatar">
+                                            <img className="rounded-full" src="https://placehold.jp/150x150.png" />
+                                        </div>
+                                        <div className="flex flex-col flex-auto ml-4">
+                                            <span className="text-md font-semibold">John O-Neil V. Geronimo</span>
+                                            <span className="text-xs text-light">jvgeronimo@up.edu.ph</span>
+                                        </div>
+                                    </div>
+                                    <div className="h-8" />
+                                    <button onClick={console.log} className="m-auto text-secondary text-base font-medium">Log out <span className="align-middle material-symbols-rounded" style={{ fontSize: 'unset' }}>arrow_right_alt</span></button>
+                                    <span className="m-auto mt-6 opacity-50 text-xs">Padayon, Isko/Iska!</span>
                                 </section>
                             </ul>
                         </div>
                     </div>
                 </section>
-                <section className="flex-row flex my-6">
+                <section className="flex-row flex">
                     <section className="flex flex-col flex-none dashboard-list-section mx-8">
                         <card className="flex-none card w-full bg-base-100 shadow-md mb-0">
                             <Application data={this.state.data[0]} />
