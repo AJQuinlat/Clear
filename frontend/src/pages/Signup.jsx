c785eb478bffd1d6448827a8414cfa80c4c8aa63
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-24 11:41:17 +0800

    feat: Make signing up work on the frontend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,10 +2,51 @@ import React from "react";
 import './Signup.css';
 
 class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onSignUp = this.onSignUp.bind(this);
+    }
+
+    onSignUp(event) {
+        event.preventDefault();
+
+        // Get the data from form
+        const formData = new FormData(event.target);        
+
+        // Construct the object based on form
+        let data = {};
+        data.firstName = formData.get("first-name");
+        data.middleName = formData.get("middle-name");
+        data.lastName = formData.get("last-name");
+        data.studentNumber = formData.get("student-number");
+        data.course = formData.get("course");
+        data.college = formData.get("college");
+        data.email = formData.get("email");
+        data.password = formData.get("password");
+        
+        fetch('http://localhost:3001/api/sign-up', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(body => {
+          if (body.success) {
+            alert("Successfully added user!");
+            event.target.reset();
+          } else {
+            alert("An error has occured. Please try again.");
+          }
+        });
+    }
+
     render() {
         return (
             <div className="signup-page flex h-screen overflow-y-scroll">
-                <form className="my-auto">
+                <form className="my-auto" onSubmit={this.onSignUp}>
                     <img className="max-h-20 px-10 my-4" src={"../assets/images/UPLB-VIG-HR-White-1.webp"} alt="UPLB Logo" />
                     <div className="px-12 justify-self-center">
                         <h1 className="text-5xl text-primary font-bold mb-2">Sign up for a Clear account</h1>
@@ -16,19 +57,19 @@ class Signup extends React.Component {
 
                         <h3 className="mb-2 text-primary text-xl font-bold leading-snug">Name</h3>
                         <div class="grid gap-2 mb-6 md:grid-cols-3">
-                            <input type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="First Name" required/>
-                            <input type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="Middle Name" required/>
-                            <input type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="Last Name" required/>
+                            <input name="first-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="First Name" required/>
+                            <input name="middle-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="Middle Name"/>
+                            <input name="last-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="Last Name" required/>
                         </div>
                             
                         <h3 className="mb-2 mt-12 text-xl text-primary font-bold leading-snug">Course and College</h3>
                         <div class="grid gap-8 mb-6 md:grid-cols-2">
                             <div className="">
-                                <input type="text" placeholder="Course/Program" className="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full mb-4" />
-                                <input type="text" placeholder="Student ID/number" className="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" />
+                                <input name="course" type="text" placeholder="Course/Program" className="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full mb-4" />
+                                <input name="student-number" type="text" placeholder="Student ID/number" className="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" />
                             </div>
 
-                            <select className="select select-bordered bg-gray-300 border border-gray-300 text-sm rounded block w-full">
+                            <select name="college" className="select select-bordered bg-gray-300 border border-gray-300 text-sm rounded block w-full">
                                 <option value="" disabled selected>College</option>
                                 <option value="CAS">CAS</option>
                                 <option value="CEAT">CEAT</option>
@@ -41,11 +82,11 @@ class Signup extends React.Component {
                         <text class="text-left text-sm">Ensure the e-mail you are using to sign-up is your UP Mail account</text>
 
                         <div class="grid gap-2 mb-6 mt-4 md:grid-cols-3">
-                            <input type="text" placeholder="Email" className="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" />
-                            <input type="password" placeholder="Password" className="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" />
+                            <input name="email" type="text" placeholder="Email" className="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" />
+                            <input name="password" type="password" placeholder="Password" className="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" />
                         </div>
 
-                        <button className="mb-2 text-base-300 btn btn-primary rounded-xl h-10 w-full">Sign-up</button>
+                        <button type="submit" className="mb-2 text-base-300 btn btn-primary rounded-xl h-10 w-full">Sign-up</button>
                         <div className="h-20" />
                     </div>
                 </form>
