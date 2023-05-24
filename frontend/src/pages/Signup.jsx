80fcb214989270ba14a4c534f0e63d1191b8ec52
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-24 12:09:28 +0800

    feat: Use DaisyUI modals for sign ups

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { Navigate } from 'react-router-dom';
 import './Signup.css';
 
 class Signup extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            closeSignUp: false,
+        }
+
         this.onSignUp = this.onSignUp.bind(this);
+        this.goToLogin = this.goToLogin.bind(this);
+    }
+
+    goToLogin() {
+        this.setState({ closeSignUp: true });
     }
 
     onSignUp(event) {
         event.preventDefault();
 
         // Get the data from form
-        const formData = new FormData(event.target);        
+        const formData = new FormData(event.target);
 
         // Construct the object based on form
         let data = {};
@@ -24,23 +34,23 @@ class Signup extends React.Component {
         data.college = formData.get("college");
         data.email = formData.get("email");
         data.password = formData.get("password");
-        
+
         fetch('http://localhost:3001/api/sign-up', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
         })
-        .then(response => response.json())
-        .then(body => {
-          if (body.success) {
-            alert("Successfully added user!");
-            event.target.reset();
-          } else {
-            alert("An error has occured. Please try again.");
-          }
-        });
+            .then(response => response.json())
+            .then(body => {
+                if (body.success) {
+                    document.getElementById('success-modal').checked = true;
+                } else {
+                    document.getElementById('error-modal').checked = true;
+                    alert("An error has occured. Please try again.");
+                }
+            });
     }
 
     render() {
@@ -51,17 +61,17 @@ class Signup extends React.Component {
                     <div className="px-12 justify-self-center">
                         <h1 className="text-5xl text-primary font-bold mb-2">Sign up for a Clear account</h1>
                         <text class="mb-12 text-left text-sm font-medium leading-1">By signing up, you agree to Clear&#39;s terms and conditions and privacy policy</text>
-                            
+
 
                         <h2 className="mb-8 mt-10 text-2xl text-primary font-bold leading-snug">Student Information</h2>
 
                         <h3 className="mb-2 text-primary text-xl font-bold leading-snug">Name</h3>
                         <div class="grid gap-2 mb-6 md:grid-cols-3">
-                            <input name="first-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="First Name" required/>
-                            <input name="middle-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="Middle Name"/>
-                            <input name="last-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="Last Name" required/>
+                            <input name="first-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="First Name" required />
+                            <input name="middle-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="Middle Name" />
+                            <input name="last-name" type="text" class="input input-bordered bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded w-full" placeholder="Last Name" required />
                         </div>
-                            
+
                         <h3 className="mb-2 mt-12 text-xl text-primary font-bold leading-snug">Course and College</h3>
                         <div class="grid gap-8 mb-6 md:grid-cols-2">
                             <div className="">
@@ -90,6 +100,29 @@ class Signup extends React.Component {
                         <div className="h-20" />
                     </div>
                 </form>
+                <input type="checkbox" id="success-modal" className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">Signed up successfully</h3>
+                        <p className="py-4">Your account has been successfully added. Welcome to Clear!</p>
+                        <div className="modal-action">
+                            <label htmlFor="success-modal" onClick={this.goToLogin} className="btn">OK</label>
+                        </div>
+                    </div>
+                </div>
+                <input type="checkbox" id="error-modal" className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">An error has occured</h3>
+                        <p className="py-4">Please try again later.</p>
+                        <div className="modal-action">
+                            <label htmlFor="error-modal" className="btn">OK</label>
+                        </div>
+                    </div>
+                </div>
+                {this.state.closeSignUp &&
+                    <Navigate to="/login" />
+                }
             </div>
         )
     }
