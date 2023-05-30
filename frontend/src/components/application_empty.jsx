abc2b97b2d66ccb356c8289d96167a0712342407
Ariezel <ambautista@up.edu.ph>
2023-05-30 16:55:57 +0800

    ui: element color changes

--- frontend/src/components/application_empty.jsx
+++ frontend/src/components/application_empty.jsx
@@ -2,11 +2,11 @@ import "material-symbols";
 
 export default function EmptyApplication(props) {
   return (
-    <div className="flex flex-col items-center justify-center h-screen" style={{ height: "90vh" }}>
-      <span className=" material-symbols-rounded" style={{ fontSize: "9rem" }}>
+    <div className="text-[#6b7280] flex flex-col items-center justify-center h-screen" style={{ height: "90vh" }}>
+      <span className="material-symbols-rounded" style={{ fontSize: "9rem" }}>
         description
       </span>
-      <span className="">Click on an application to view its details</span>
+      <span>Click on an application to view its details</span>
     </div>
   );
 }
