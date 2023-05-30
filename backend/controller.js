f65c6772ed06a0318aa665cec638469a0adc5b3b
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-30 17:46:30 +0800

    feat: Introduce a one-size-fits-all API for logged in checks and data fetching
    The 'heartbeat' API will be used by the app to get all user information, necessary data, and in the future more information that the app needs all at once so we can only single-handedly call this API instead.
    
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

--- backend/controller.js
+++ backend/controller.js
@@ -65,6 +65,60 @@ const logIn = async (req, res) => {
   })
 }
 
+async function getUser(req) {
+  // Scenario 1 - No cookies / no authToken cookie sent
+  if (!req.cookies || !req.cookies.authToken) {
+    return null;
+  }
+  
+  try {
+    // try to verify the token
+    const tokenPayload = jwt.verify(req.cookies.authToken, 'THIS_IS_A_SECRET_STRING');
+
+    // check if the _id in the payload is an existing user id
+    const user = await User.findById(tokenPayload._id);
+
+    if (user) {
+      // SUCCESS Scenario - User is found
+      return user;
+    } else {
+      // FAIL Scenario 2 - Token is valid but user id not found
+      return null;
+    }
+  } catch {
+    // FAIL Scenario 3 - Error in validating token / Token is not valid
+    return null;
+  }
+}
+
+const heartbeat = async (req, res) => {
+  let data = {};
+  data.userInfo = await getUser(req);
+
+  // Check if token is valid and user is logged in
+  if (data.userInfo === null) {
+    return res.send(data);
+  }
+
+  switch (data.userInfo.userType) {
+    case "STUDENT":
+      data.applications = await Application.find({ uid: data.userInfo._id });
+      break;
+    case "ADVISER":
+      data.applications = await Application.find({ adviserUid: data.userInfo._id });
+      break;
+    case "CLEARANCE_OFFICER":
+      data.applications = await Application.find({ officerUid: data.userInfo._id });
+      break;
+    case "ADMINISTRATOR":
+      data.applications = await Application.find({});
+      break;
+  }
+
+  console.log(data);
+  return res.send(data);
+}
+
 const checkIfLoggedIn = async (req, res) => {
 
   if (!req.cookies || !req.cookies.authToken) {
